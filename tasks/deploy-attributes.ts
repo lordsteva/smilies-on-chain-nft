4075194e0eb3cd703b5d942ccb124d1e595d07a4
lordsteva <lordsteva@hotmail.com>
2022-03-08 20:10:20 +0100

    add eyes

--- tasks/deploy-attributes.ts
+++ tasks/deploy-attributes.ts
@@ -7,7 +7,7 @@ import path from "path";
 task("deploy-attributes", "Deploys all attribute contracts").setAction(
   async (args, env) => {
     try {
-      const attributes = ["Background", "Face"];
+      const attributes = ["Background", "Face", "Eyes"];
       const promises = attributes.map((attribute) => {
         return deploy({ attribute }, env);
       });
